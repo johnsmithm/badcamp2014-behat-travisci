2cd6bfd4812af051b7ffa84e3d506184f52af693
Mosnoi Ion <johnsmithm@users.noreply.github.com>
2017-08-12 17:19:28 +0200

    composer install drupal

--- bin/install_drupal_mysql.sh
+++ bin/install_drupal_mysql.sh
@@ -7,13 +7,19 @@
 # up one of these for you by default.
 #
 rm -rf html
-$BASEDIR/vendor/bin/drush help make
+#$BASEDIR/vendor/bin/drush help make
 $BASEDIR/vendor/bin/drush -v
 #$BASEDIR/vendor/bin/drush make -y  drupal.make html
-mkdir html
+#mkdir html
+#$BASEDIR/vendor/bin/drush dl drupal-7.x
+composer create-project drupal-composer/drupal-project:7.x-dev html --stability dev --no-interaction
 cd html
-$BASEDIR/vendor/bin/drush dl drupal-7.x
 ls
+drush composer-generate @site
+composer install
+cd web
+drush -y core-quick-drupal
+cd ..
 $BASEDIR/vendor/bin/drush si -y standard --db-url=mysql://root@localhost/drupal --account-name=admin --account-pass=admin
 
 # We might do the same thing with drush quick-drupal
